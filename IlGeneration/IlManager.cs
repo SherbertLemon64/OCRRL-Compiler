6d967a85b4a307c2b49808d453dbf74b3629e71d
Sherbert Lemon <thomasgriffgoo@gmail.com>
2022-02-17 13:36:40 +0000

    Added an IlManager to keep track of the line address

---
+++ IlGeneration/IlManager.cs
@@ -0,0 +1,27 @@
+﻿namespace OCRRFcompiler.IlGeneration
+{
+	public class IlManager
+	{
+		public long address { get; private set; }
+
+		public string GetFormattedAddressAndIncrement()
+		{
+			string returnValue = $"IL_{address.ToString("X")}:";
+			address++;
+			return returnValue;
+		}
+
+		public long GetAddressAndIncrement()
+		{
+			long t_address = address;
+			address++;
+			
+			return t_address;
+		}
+
+		public static string FormatAddress(long _address)
+		{
+			return $"IL_{_address.ToString("X")}";
+		}
+	}
+}
