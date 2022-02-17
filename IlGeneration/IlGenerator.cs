0a39705a23a5b810f884208466a6bc31380fab8c
Sherbert Lemon <thomasgriffgoo@gmail.com>
2022-02-17 13:41:54 +0000

    Added a wrapper for statement Il Generation

---
+++ IlGeneration/IlGenerator.cs
@@ -0,0 +1,17 @@
+﻿using OCRRFcompiler.Parsing;
+
+namespace OCRRFcompiler.IlGeneration
+{
+	public class IlGenerator
+	{
+		private SyntaxTree Tree;
+
+		public string GenerateIl(SyntaxTree _tree)
+		{
+			Tree = _tree;
+			IlManager manager = new IlManager();
+
+			return _tree.GlobalScope.GenerateIl(manager);
+		}
+	}
+}
