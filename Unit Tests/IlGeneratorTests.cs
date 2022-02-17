e17430afa8c4b8a5247474d78284f20e9505a419
Sherbert Lemon <thomasgriffgoo@gmail.com>
2022-02-17 13:42:09 +0000

    Added unit tests for IlGeneration

---
+++ Unit Tests/IlGeneratorTests.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using NUnit.Framework;
+using OCRRFcompiler.IlGeneration;
+using OCRRFcompiler.Parsing;
+
+namespace OCRRFcompiler.Testing
+{
+	
+	public class IlGeneratorTests
+	{
+		private IlGenerator generator = new IlGenerator();
+
+		private Parser parser = new Parser();
+		
+		[Test]
+		[Ignore("Takes 30ms")]
+		public void ExternalGeneratorTest()
+		{
+			parser.ParseLocation(Directory.GetCurrentDirectory() + @"\Test.rl");
+			
+			string il = generator.GenerateIl(parser.Tree);
+			Assert.Pass();
+		}
+
+		[Test]
+		public void TestForLoop()
+		{
+			string forLoop = 
+				@"
+				y = 0
+				for x=0 to 100 step 2
+					y = 2
+				next x";
+			parser.Parse(forLoop);
+			
+			string il = generator.GenerateIl(parser.Tree);
+		}
+
+		[Test]
+		public void TestWhileLoop()
+		{
+			string whileLoop = 
+				@"
+				y = 0
+				while y<10 AND y > 5
+					y = y + 1
+				endwhile";
+			parser.Parse(whileLoop);
+
+			string il = generator.GenerateIl(parser.Tree);
+		}
+	}
+}
