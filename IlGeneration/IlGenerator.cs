755c6c6d7c24e3c947d07445e2065096c881cc69
Sherbert Lemon <thomasgriffgoo@gmail.com>
2022-03-05 16:48:28 +0000

    Added main method generation

--- IlGeneration/IlGenerator.cs
+++ IlGeneration/IlGenerator.cs
@@ -20,7 +20,7 @@ namespace OCRRFcompiler.IlGeneration
 
 			string locals = GenerateLocals();
 			
-			return null;
+			return GenerateMainMethod(locals + ilCode);
 		}
 
 		public string GenerateLocals()
@@ -36,6 +36,46 @@ namespace OCRRFcompiler.IlGeneration
 			returnValue += ")";
 			
 			return returnValue;
+		}
+
+		public string GenerateMainMethod(string _code)
+		{
+			return @"
+					.assembly extern System.Runtime
+					{
+					  .publickeytoken = (B0 3F 5F 7F 11 D5 0A 3A )
+					  .ver 5:0:0:0
+					}
+					.assembly extern System.Console
+					{
+					  .publickeytoken = (B0 3F 5F 7F 11 D5 0A 3A )                       
+					  .ver 5:0:0:0
+					}
+					.assembly _
+					{
+						.custom instance void [System.Private.CoreLib]System.Runtime.CompilerServices.CompilationRelaxationsAttribute::.ctor(int32) = (
+					        01 00 08 00 00 00 00 00
+					    )
+					}
+					.class private auto ansi abstract sealed beforefieldinit '<Program>$'
+						extends [System.Private.CoreLib]System.Object
+					{
+						.custom instance void [System.Private.CoreLib]System.Runtime.CompilerServices.CompilerGeneratedAttribute::.ctor() = (
+						01 00 00 00
+						)
+						
+						.method private hidebysig static 
+							void '<Main>$' (
+								string[] args
+							) cil managed 
+						{
+							.maxstack 3
+							.entrypoint
+							" + _code + @"
+						} // end of method '<Program>$'::'<Main>$'
+
+					} // end of class <Program>$
+					";
 		}
 	}
 }
